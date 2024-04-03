a41daf76d6afa542628d7e9026b1cac55dca6a74
mcarrobo24 <mcaf.dev24@gmail.com>
2024-04-02 20:44:38 -0500

    Data-base was changed

--- connect.js
+++ connect.js
@@ -4,14 +4,16 @@ const {MongoClient} =require('mongodb');
 const client = new MongoClient(config.dbUrl);
 
 // eslint-disable-next-line no-unused-vars
-//const { dbUrl } = config;
+const { dbUrl } = config;
 
 async function connect() {
   // TODO: Database Connection
   try{
+    //Conecta a MONGODB
     await client.connect();
-    const db= client.db('Colegios');
-    console.log("conexion correct")
+    //Se coloca el nombre de la BD
+    const db= client.db('burger_queen');
+    console.log("conexion correcta");
     return db;
 
   } catch(error){
