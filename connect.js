5f33c93592fa328167f8480a3f810ff95bf31cba
mcarrobo24 <mcaf.dev24@gmail.com>
2024-04-08 14:50:36 -0500

    comments added

--- connect.js
+++ connect.js
@@ -1,10 +1,10 @@
 const config = require('./config');
 const {MongoClient} =require('mongodb');
-
+//se pasa la url de la BD 
 const client = new MongoClient(config.dbUrl);
 
 // eslint-disable-next-line no-unused-vars
-const { dbUrl } = config;
+//const { dbUrl } = config;
 
 async function connect() {
   // TODO: Database Connection
